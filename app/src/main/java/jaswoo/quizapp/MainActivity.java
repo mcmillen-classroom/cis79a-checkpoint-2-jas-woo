d1b7559bece1c159536e2e4f11ab3056061e77fa
jaswoo <woodformed@gmail.com>
2019-06-24 11:48:01 -0700

    Starting Checkpoint 2. INITIAL COMMIT.

---
+++ app/src/main/java/jaswoo/quizapp/MainActivity.java
@@ -0,0 +1,76 @@
+package jaswoo.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mTextView;
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+    private TextView mFeedback;
+
+
+    private Question[] mQuestions;
+    private int mIndex;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFeedback = (TextView) findViewById(R.id.feedback_textview);
+
+        mTrueButton.setOnClickListener(this);
+        mFalseButton.setOnClickListener(this);
+        mFeedback.setOnClickListener(this);
+
+        mTextView = (TextView) findViewById(R.id.text_view);
+
+        //INITIALIZE AN ARRAY OF QUESTIONS
+        mQuestions = new Question[5];
+        mIndex = 0;
+        //INITIAIZE EACH SLOT IN THE ARRAY
+        mQuestions[0] = new Question(R.string.question_1, true);
+        mQuestions[1] = new Question(R.string.question_2, false);
+        mQuestions[2] = new Question(R.string.question_3, false);
+        mQuestions[3] = new Question(R.string.question_4, true);
+        mQuestions[4] = new Question(R.string.question_5, false);
+
+
+        mTextView.setText(mQuestions.getTextResId());
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if (view.getId() == R.id.true_button && mQuestions.getAnswer() == true) {
+            Toast myToast = Toast.makeText(this, "YOU ARE CORRECT!", Toast.LENGTH_SHORT);
+            myToast.setGravity(Gravity.TOP, 0, 0);
+            myToast.show();
+        }
+        else if (view.getId() == R.id.false_button && mQuestions.getAnswer() == false) {
+            Toast myToast = Toast.makeText(this, "YOU ARE CORRECT!", Toast.LENGTH_SHORT);
+            myToast.setGravity(Gravity.TOP, 0, 0);
+            myToast.show();
+        }
+        else {
+            Toast myToast = Toast.makeText(this, "You are incorrect😟", Toast.LENGTH_SHORT);
+            myToast.setGravity(Gravity.TOP, 0, 0);
+            myToast.show();
+        }
+
+    }
+}
---
+++ app/src/main/java/jaswoo/quizapp/Question.java
@@ -0,0 +1,27 @@
+package jaswoo.quizapp;
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswer;
+
+    public Question(int textResId, boolean answer) {
+        mTextResId = textResId;
+        mAnswer = answer;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean getAnswer() {
+        return mAnswer;
+    }
+
+    public void setAnswer(boolean answer) {
+        mAnswer = answer;
+    }
+}
